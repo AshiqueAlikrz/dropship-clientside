749f413e46244abf258062e0520e0f78c11291af
Ashique Ali K <ashiquealikrz9@gmail.com>
2023-12-02 18:17:20 +0530

    first commit

---
+++ src/pages/home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../style/home.css";
+
+const home = () => {
+  return (
+    <div className="home-background">
+      <div className="curesol-main-div">
+        <div className="home-text-main-div">
+          <h1 className="home-page-text">Welcome to MyShopy</h1> <h2 className="home-page-text2"> Your Ultimate Shopping Destination!</h2>
+        </div>
+        <img
+          className="home-curesol-image"
+          src="https://www.freepnglogos.com/uploads/shoes-png/shoes-png-srinivascreations-deviantart-1.png"
+          alt=""
+        ></img>
+      </div>
+    </div>
+  );
+};
+
+export default home;
