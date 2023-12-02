94ed661da5136d2ca574cfb90916ba6457edc5f6
Ashique Ali K <ashiquealikrz9@gmail.com>
2023-12-03 02:46:54 +0530

    navbar updated

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import "../style/navbar.css";
 
 const navbar = () => {
   return (
-    <div>
-      
+    <div className="navbar-main-div">
+      <div className="navbar-div">
+        <div className="list-name">
+          <ul>
+            <li>Home</li>
+            <li>Shop</li>
+            <li>Blog</li>
+            <li>Pages</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default navbar
+export default navbar;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "../style/home.css";
+import Navbar from "../components/navbar"
 
 const home = () => {
   return (
     <div className="home-background">
+      <Navbar />
       <div className="curesol-main-div">
         <div className="home-text-main-div">
           <h1 className="home-page-text">Welcome to MyShopy</h1> <h2 className="home-page-text2"> Your Ultimate Shopping Destination!</h2>
